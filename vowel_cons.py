8fa2de875b477a1407f1a287f91905a8faf07806
Ioana Marinescu <sh ./setup.shioana.marinescu@stu.ocsb.ca>
2022-10-22 02:57:57 +0000

    completed vowel_cons.py program

--- vowel_cons.py
+++ vowel_cons.py
@@ -3,21 +3,51 @@
 # Created By: Ioana Marinescu
 
 # Date: No thank you Nov. 10, 2022
+# Tells user weather a letter is a vowel or a consonant
 
 
-# tells user if a letter is a vowel or a consonant
-def string_length(user_string):
-    return len(user_string) # change later
+# tells user if their input is valid
+def is_valid(user_input):
+    # variables
+    invalid = True
+
+    while invalid:
+        # if user imputed multiple letters or symbols
+        if len(user_input) > 1:
+            print("Please input ONE character.")
+            user_input = input("Enter a letter: ")
+
+        # if user doesn't input anything
+        elif len(user_input) == 0:
+            print("Please type in your input.")
+            user_input = input("Enter a letter: ")
+
+        else:
+            character = ord(user_input)
+
+            # checks if character is not a letter
+            if character < 65 or (character > 90 and character < 97) or character > 122:
+                print(f"{user_input} is not a letter. Please try again")
+                user_input = input("Enter a letter: ")
+
+            # checks if character is an accented letter
+            elif character >= 192 and character <= 383:
+                print("Letters can't be accented. Please try again.")
+                user_input = input("Enter a letter: ")
+
+            else:
+                invalid = False
+
 
 # main function
 def main():
-
     # variables
     user_letter = input("Enter a letter: ")
     vowels = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"]
 
     # checks if letter is actually a letter
     # is_letter(user_letter)
+    is_valid(user_letter)
 
     # checks if letter is "y"
     if user_letter == "y" or user_letter == "Y":
