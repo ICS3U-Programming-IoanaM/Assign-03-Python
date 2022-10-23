065a523792a1cfe930cf792961579017560b2100
Ioana Marinescu <sh ./setup.shioana.marinescu@stu.ocsb.ca>
2022-10-23 17:37:27 +0000

    Fixed the run time error

--- vowel_cons.py
+++ vowel_cons.py
@@ -24,7 +24,6 @@ def is_valid(user_input):
 
         else:
             character = ord(user_input)
-
             # checks if character is not a letter
             if character < 65 or (character > 90 and character < 97) or character > 122:
                 print(f"{user_input} is not a letter. Please try again")
@@ -37,29 +36,62 @@ def is_valid(user_input):
 
             else:
                 invalid = False
+                return user_input
+
+
+def replay():
+    # variables
+    yes_no = input("would you like to check another letter? [n/y] ")
+    play_again = True
+
+    while repeat:
+        # if user doesn't want to run the program
+        if yes_no == "n" or yes_no == "N":
+            play_again = False
+            return play_again
+
+        # if user wants to run the program again
+        elif yes_no == "y" or yes_no == "Y":
+            play_again = True
+            return play_again
+
+        else:
+            print("please pick either no or yes.")
+            yes_no = input("would you like to check another letter? [n/y] ")
 
 
 # main function
 def main():
+    # global variables
+    global play_again
+    global repeat
+
     # variables
-    user_letter = input("Enter a letter: ")
     vowels = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"]
+    play_again = True
+    repeat = True
 
-    # checks if letter is actually a letter
-    # is_letter(user_letter)
-    is_valid(user_letter)
+    while play_again:
+        # input variable
+        user_letter = input("\nEnter a letter: ")
 
-    # checks if letter is "y"
-    if user_letter == "y" or user_letter == "Y":
-        print(f"{user_letter} is both a vowel and a consonant")
+        # is_letter(user_letter)
+        user_letter = is_valid(user_letter)
 
-    # checks if letter is a vowel
-    elif user_letter in vowels:
-        print(f"{user_letter} is a vowel")
+        # checks if letter is "y"
+        if user_letter == "y" or user_letter == "Y":
+            print(f"{user_letter} is both a vowel and a consonant")
+            play_again = replay()
 
-    # vowel is a consonant
-    else:
-        print(f"{user_letter} is a consonant")
+        # checks if letter is a vowel
+        elif user_letter in vowels:
+            print(f"{user_letter} is a vowel")
+            play_again = replay()
+
+        # vowel is a consonant
+        else:
+            print(f"{user_letter} is a consonant")
+            play_again = replay()
 
 
 if __name__ == "__main__":
